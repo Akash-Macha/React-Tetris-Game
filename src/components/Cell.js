a0ee5f49dc29e9c493a868bd33e5a105b2df3a5c
Akash-Macha <akashmacha1999@gmail.com>
2020-06-10 14:55:28 +0530

    added styling 🔥

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from "./tetromios";
 
-const Cell = () => (<p>CEll</p>);
+const Cell = ({ type }) => (
+    <StyledCell type={'L'} color={TETROMINOS['L'].color}>
+    </StyledCell>
+);
 
 export default Cell;
--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Cell from './Cell';
+import { StyledStage } from './styles/StyledState';
 
 const Stage = ({ stage }) => (
-    <div>
-        {console.log('stage: ', stage)}
+    // width and height props changed the game
+    <StyledStage width={stage[0].length} height={stage.length}>
         {stage.map((row) => row.map((cell, x) => <Cell />) )  }
-    </div>
+    </StyledStage>
 );
 
 export default Stage;
