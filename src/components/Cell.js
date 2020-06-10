2398364242871bb51346e2fa6d75aedd57c1a4f9
Akash-Macha <akashmacha1999@gmail.com>
2020-06-10 15:00:44 +0530

    styled Display (awesome🔥)

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,8 +3,7 @@ import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from "./tetromios";
 
 const Cell = ({ type }) => (
-    <StyledCell type={'L'} color={TETROMINOS['L'].color}>
-    </StyledCell>
+    <StyledCell type={'L'} color={TETROMINOS['L'].color} />
 );
 
 export default Cell;
